592b92497eb4457abd71250c6a6af495a4cacf54
Dmitry Verkhoturov <dmitry.verkhoturov@booking.com>
2019-04-16 15:08:08 +0200

    bump gin-go-metrics

--- server/server.go
+++ server/server.go
@@ -10,7 +10,8 @@ import (
 
 	"github.com/GeertJohan/go.rice"
 	"github.com/bmc-toolbox/actor/routes"
-	"github.com/bmc-toolbox/gin-go-metrics"
+	metrics "github.com/bmc-toolbox/gin-go-metrics"
+	"github.com/bmc-toolbox/gin-go-metrics/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -50,7 +51,7 @@ func Serve() {
 	router := gin.Default()
 
 	if viper.GetBool("metrics.enabled") {
-		err := gin_metrics.Setup(
+		err := metrics.Setup(
 			viper.GetString("metrics.type"),
 			viper.GetString("metrics.host"),
 			viper.GetInt("metrics.port"),
@@ -61,9 +62,9 @@ func Serve() {
 			fmt.Printf("Failed to set up monitoring: %s", err)
 			os.Exit(1)
 		}
-		go gin_metrics.Scheduler(time.Minute, gin_metrics.GoRuntimeStats, []string{""})
-		go gin_metrics.Scheduler(time.Minute, gin_metrics.MeasureRuntime, []string{"uptime"}, time.Now())
-		p := gin_metrics.NewMetrics([]string{})
+		go metrics.Scheduler(time.Minute, metrics.GoRuntimeStats, []string{""})
+		go metrics.Scheduler(time.Minute, metrics.MeasureRuntime, []string{"uptime"}, time.Now())
+		p := middleware.NewMetrics([]string{})
 		router.Use(p.HandlerFunc())
 	}
 
